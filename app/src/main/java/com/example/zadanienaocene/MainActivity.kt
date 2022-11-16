33553bb47ca4ed2ba5b254d84ab227423d6f75db
piasek <piaskowski3456@gmail.com>
2022-11-16 12:39:35 +0100

    sxsx

--- app/src/main/java/com/example/zadanienaocene/MainActivity.kt
+++ app/src/main/java/com/example/zadanienaocene/MainActivity.kt
@@ -4,46 +4,77 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fun silnia(x: Int, y: Int): Int
+        {
+            var x_ = x
+            var y_ = y
 
-        var flaga = 0
+            if (y_ != x_) {
+                x_ = x_ * (x_ + 1)
+                y_++
+                silnia(x_, y_)
+
+            }
+            return x_
+        }
+
+
+        var flaga = 1
+        var liczba = 0
 
         findViewById<Button>(R.id.back).setOnClickListener {
-            if(flaga==0) {
+            if(flaga==1) {
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.katjuszyc)
-                flaga = 1
+                flaga = 2
             }else{
-                if(flaga==1){
+                if(flaga==2){
                     findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.katjuszyczczapko)
-                    flaga = 2
+                    flaga = 3
                 }else{
-                    if(flaga==2){
+                    if(flaga==3){
                         findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.pancerfuuuuuuuuuur)
-                        flaga=0
+                        flaga=1
                     }
                 }
             }
         }
         findViewById<Button>(R.id.next).setOnClickListener {
-            if(flaga==0) {
+            if(flaga==1) {
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.katjuszyc)
-                flaga = 1
+                flaga = 2
             }else{
-                if(flaga==1){
+                if(flaga==2){
                     findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.katjuszyczczapko)
-                    flaga = 2
+                    flaga = 3
                 }else{
-                    if(flaga==2){
+                    if(flaga==3){
                         findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.pancerfuuuuuuuuuur)
-                        flaga=0
+                        flaga=1
                     }
                 }
             }
+        }
+        findViewById<Button>(R.id.DodawanieSilni).setOnClickListener {
+            if(liczba<10){
+                liczba += 1
+                findViewById<TextView>(R.id.liczbasilni).text = liczba.toString()
+            }
+        }
+        findViewById<Button>(R.id.OdejmowanieSilni).setOnClickListener {
+            if(liczba>0){
+                liczba -= 1
+                findViewById<TextView>(R.id.liczbasilni).text = liczba.toString()
+            }
+        }
+        findViewById<Button>(R.id.buttonlsilnia).setOnClickListener {
+            findViewById<TextView>(R.id.wynik).text = silnia(liczba, liczba).toString()
         }
     }
 }
